a6e46a3f77f1eede39f0eb7d2d763f5d0dcae5f1
tomhall82 <tom.hall82@yahoo.co.uk>
2024-08-30 12:16:52 +0100

    updated index http response

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == "POST":
+        return HttpResponse("Hello, world!")
+    else:
+        return HttpResponse(request.method)
